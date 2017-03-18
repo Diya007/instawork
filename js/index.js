caf7a49d7c3198acd26458dd0585f88d0d5ca89f
Diya Zhao <zdy.gogo@msn.com>
2017-03-17 18:16:31 -0700

    initial test, make hello world work

--- js/index.js
+++ js/index.js
@@ -1,2 +1,9 @@
 require('babel-polyfill');
 
+import React from 'react';
+import ReactDOM from 'react-dom';
+import List from './components/list';
+
+document.addEventListener('DOMContentLoaded', () => {
+	ReactDOM.render(<List />, document.getElementById('app'));
+})
